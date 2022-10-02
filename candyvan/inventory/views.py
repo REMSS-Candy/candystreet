87fd44f22e9b14f8c21dcf63c0ff1acdc6f5abce
KokoseiJ <kokoseij@gmail.com>
2022-10-02 00:11:52 -0700

    Simple Login/Logout implemented, PLEASE change details before production

--- candyvan/inventory/views.py
+++ candyvan/inventory/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth import authenticate
+
+from .forms import LoginForm
+
 
 def index(request):
     return HttpResponse("Hello world!")
+
+
+def login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                request.session['user_id'] = user.id
+    else:
+        form = LoginForm()
+
+    return render(request, 'inventory/login.html',
+                  {'form': form, 'user': request.session.get("user_id")})
+
+
+def logout(request):
+    empty = object()
+    if request.session.get("user_id", empty) is not empty:
+        del request.session['user_id']
+
+    return redirect("login")
