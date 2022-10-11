62ab4875415534627ccc69107a181123c4f99055
AidenDeveloper <aidennara@gmail.com>
2022-10-11 14:38:36 -0700

    Update admin.py

--- candyvan/inventory/admin.py
+++ candyvan/inventory/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CandyUser
+from .models import CandyUser, Item, Sale, Transaction, Revenue, Log
 
 admin.site.register(CandyUser, UserAdmin)
+admin.site.register(Item)
+admin.site.register(Sale)
+admin.site.register(Transaction)
+admin.site.register(Revenue)
+admin.site.register(Log)
