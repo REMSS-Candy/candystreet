dfef60665a96f8a52407fc4e889f1c6417654e86
KokoseiJ <kokoseij@gmail.com>
2022-10-11 14:39:00 -0700

    Changed /inventory from hello world to redirection

--- candyvan/inventory/urls.py
+++ candyvan/inventory/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', RedirectView.as_view(url='/inventory/login', permanent=False),
+         name='inventory_index_login_redir'),
     path('login/', views.user.login, name='login'),
     path('logout/', views.user.logout, name='logout'),
     path('sell/', views.sell.sell, name='sell')
