81b95312aa59b0b1a57a5ad9861be451f9f2a248
KokoseiJ <kokoseij@gmail.com>
2022-10-16 04:57:18 -0700

    Added ItemHistory to admin page

--- candyvan/inventory/admin.py
+++ candyvan/inventory/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CandyUser, Item, Sale, Transaction, Revenue, Log
+from .models import CandyUser, Item, Sale, Transaction, Revenue, ItemHistory
 
 admin.site.register(CandyUser, UserAdmin)
 admin.site.register(Item)
 admin.site.register(Sale)
 admin.site.register(Transaction)
 admin.site.register(Revenue)
-admin.site.register(Log)
+admin.site.register(ItemHistory)
